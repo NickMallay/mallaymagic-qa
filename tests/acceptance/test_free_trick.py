2899c27befcd5e37ef37d44963965d8e27ad8122
Nick <nickmallay@hotmail.com>
2026-04-23 17:23:19 -0400

    test: add test for invalid email in freebee emailgate - fails correctly

--- tests/acceptance/test_free_trick.py
+++ tests/acceptance/test_free_trick.py
@@ -21,4 +21,8 @@ def test_free_trick_hidden_until_email_submitted(page):
     page.goto("https://www.mallaymagic.com/free-trick")
     expect(page.get_by_test_id("free-trick-content")).to_be_hidden()
     expect(page.get_by_test_id("email-gate")).to_be_visible()
+def test_email_failure_confirmation(page):
+    page.goto("https://www.mallaymagic.com/free-trick")
+    page.get_by_role("button", name="Submit").click()
+    expect(page.get_by_text("Please enter a valid email address.")).to_be_visible()
 # User Flow informed tests 
