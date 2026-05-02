9038999f03bc6109846933f53d298d099d60d417
NickMallay <nickmallay@hotmail.com>
2026-05-02 11:03:19 -0400

    test: add 404 summary test, test failing correctly as site not built

--- tests/acceptance/test_404.py
+++ tests/acceptance/test_404.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page
 from playwright.sync_api import expect
 
-## Basic page functionality
-## Feature specefic tests 
-## User Flow informed tests 
+def test_404_has_summary(page):
+    page.goto("https://www.mallaymagic.com/404")
+    expect(page.get_by_test_id("404-summary")).to_be_visible()
